98339c45a111cfd6cb76a0de54f1341f7e474529
Marco Li <marcoli434ovo@gmail.com>
2026-09-15 11:14:08 +0800

    feat: Lab1 requirement

---
+++ src/main/java/Lab1/mainApp1.java
@@ -0,0 +1,11 @@
+package Lab1;
+
+public class mainApp1 {
+    public static void main(String[] args) {
+        int b = 4;
+        int n = 5;
+        System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
+        System.out.println(n+"! = " + myLibrary.factorial(n));
+        System.out.println("Program ..Ended …");
+    }
+}
