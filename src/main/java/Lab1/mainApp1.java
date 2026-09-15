fd0d9f9dfe183072c2fda83167aee2421d19f125
Marco Li <marcoli434ovo@gmail.com>
2026-09-15 11:18:00 +0800

    add: 2 printing lines

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -4,6 +4,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 4;
         int n = 5;
+        System.out.println("Program ..Starting...");
+        System.out.println("1 + 1 = "+2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
